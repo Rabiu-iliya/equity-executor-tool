2be3d85760823b27c0a2db7124ada6bb600a4884
Rabiu Iliya <rabiuiliya30@gmail.com>
2026-02-19 12:52:31 +0100

    updated main.tsx to add router

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { CurrencyProvider } from "./context/currencyContext.jsx";
 
+<CurrencyProvider>
+  <App />
+</CurrencyProvider>
 createRoot(document.getElementById("root")!).render(<App />);
